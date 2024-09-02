7d5e78d6202b8b3ffba94ecd200cb4382c6e1ca5
haha i'm in danger <squeeze@fmls-MacBook-Pro.local>
2024-09-02 23:23:39 +0500

    new lazyvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+
+  -- modicator (auto color line number based on vim mode)
+  {
+    "mawkler/modicator.nvim",
+    dependencies = "scottmckendry/cyberdream.nvim",
+    init = function()
+      -- These are required for Modicator to work
+      vim.o.cursorline = false
+      vim.o.number = true
+      vim.o.termguicolors = true
+    end,
+    opts = {},
+  },
+}
